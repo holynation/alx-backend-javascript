1c6d2e69370d058929df86df9e4bd260edbb698a
Alatise Oluwaseun <holynation667@gmail.com>
2023-01-30 18:09:34 +0100

    feat: Added building class

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,14 +1,14 @@
+/* eslint-disable no-underscore-dangle */
 class Building {
   constructor(sqft) {
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
 
